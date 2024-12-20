017a29544eddacf3a5ec21a74fe6a90d0227528b
Arthur Gan <arthurgan29@gmail.com>
2024-12-19 16:52:41 -0800

    added particle toggler

--- VFX-Workshop/Assets/Base/Scripts/ParticleGroup.cs
+++ VFX-Workshop/Assets/Base/Scripts/ParticleGroup.cs
@@ -1,16 +1,52 @@
+using System;
 using System.Collections.Generic;
 using Gdac3PWorkshopPack.Annotating.Scripts;
 using UnityEngine;
 
 public class ParticleGroup : MonoBehaviourDevNote
 {
+    public static bool Visible;
+    public static Action<bool> OnVisibleChanged;
+
     [SerializeField]
     private List<ParticleSystem> _particleSystems;
+
+    private void Awake()
+    {
+        OnVisibleChanged += OnPlayParticlesChangedHandler;
+
+        OnPlayParticlesChangedHandler(Visible);
+    }
+
+    private void OnDestroy()
+    {
+        OnVisibleChanged -= OnPlayParticlesChangedHandler;
+    }
 
     private void OnValidate()
     {
         _particleSystems = new List<ParticleSystem>();
         _particleSystems.AddRange(GetComponentsInChildren<ParticleSystem>(true));
+    }
+
+    private void OnPlayParticlesChangedHandler(bool enabled)
+    {
+        if (!enabled)
+        {
+            foreach (ParticleSystem particleSystem in _particleSystems)
+            {
+                ParticleSystem.MainModule main = particleSystem.main;
+                main.maxParticles = 0;
+            }
+        }
+        else
+        {
+            foreach (ParticleSystem particleSystem in _particleSystems)
+            {
+                ParticleSystem.MainModule main = particleSystem.main;
+                main.maxParticles = 10000;
+            }
+        }
     }
 
     public void Play()
---
+++ VFX-Workshop/Assets/Base/Scripts/ParticleToggle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Base.Scripts
+{
+    /// <summary>
+    ///     Globally toggles particle groups on and off, for demonstration purposes.
+    /// </summary>
+    public class ParticleToggle : MonoBehaviour
+    {
+        [SerializeField]
+        private Toggle _toggle;
+
+        private void Awake()
+        {
+            _toggle.onValueChanged.AddListener(OnToggleValueChanged);
+            OnToggleValueChanged(_toggle.isOn);
+        }
+
+        private void OnDestroy()
+        {
+            _toggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+        }
+
+        private void OnToggleValueChanged(bool toggle)
+        {
+            ParticleGroup.Visible = toggle;
+            ParticleGroup.OnVisibleChanged?.Invoke(toggle);
+        }
+    }
+}
